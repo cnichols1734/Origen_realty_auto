bb2a121e98cb8c270eacb123467ba8b9e3b24c10
cnichols1734 <cnichols@beyondfinance.com>
2022-12-22 18:29:23 -0600

    Added a page scroll function

--- origen_realty.py
+++ origen_realty.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 # Chrome driver Path
@@ -11,31 +9,39 @@ driver = webdriver.Chrome(PATH)
 driver.get('https://origenrealty.com')
 
 # Sleep to allow site to load
-time.sleep(3)
+time.sleep(.5)
 
 # Click the "Contact" menu item
 driver.find_element(By.XPATH, '//*[@id="menu-item-273"]/a').click()
 
 # Sleep to allow site to load
-time.sleep(4)
+time.sleep(1)
+
+# Scroll down 1/5 of the page
+scroll_amount = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, "
+                                      "document.documentElement.clientHeight, document.documentElement.scrollHeight, "
+                                      "document.documentElement.offsetHeight );") / 5
+driver.execute_script(f"window.scrollTo(0, {scroll_amount});")
 
 # Find the form elements
 name_field = driver.find_element(By.XPATH, '//*[@id="wpcf7-f271-p272-o1"]/form/div[2]/span[1]/input')
 email_field = driver.find_element(By.XPATH, '//*[@id="wpcf7-f271-p272-o1"]/form/div[2]/div/span[1]/input')
 phone_field = driver.find_element(By.XPATH, '//*[@id="wpcf7-f271-p272-o1"]/form/div[2]/div/span[2]/input')
 message_field = driver.find_element(By.XPATH, '//*[@id="wpcf7-f271-p272-o1"]/form/div[2]/span[2]/input')
+other_comments = driver.find_element(By.XPATH,'//*[@id="wpcf7-f271-p272-o1"]/form/div[2]/span[3]/textarea')
 
 # Enter the form values
-name_field.send_keys("test")
+name_field.send_keys("Test Software")
 email_field.send_keys("Testuser@mail.com")
 phone_field.send_keys("7777777777")
-message_field.send_keys("test message")
+message_field.send_keys("Test message")
+other_comments.send_keys("This is written by automated software :)")
 
 # Find the submit button element
 submit_button = driver.find_element(By.XPATH, "//*[@id='wpcf7-f271-p272-o1']/form/div[2]/input")
 
 # Click the submit button
-submit_button.click()
+#submit_button.click()
 
 # To keep chrom open
 time.sleep(30)
